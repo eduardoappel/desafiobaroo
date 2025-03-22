9a2f86457fd24ae3cd0dcf07dce6dd988b3bdee3
eduardoappel <eduardo.appel@gmail.com>
2025-03-22 15:10:44 -0300

    Modificado application ler os dados cliente do bar

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,30 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("DESAFIO: Bar OO ");
+
+		System.out.println("Digite sexo (F ou M): ");
+		char sexo = sc.next().charAt(0);
+		System.out.println("Digite a quantidade de cervejas: ");
+		int cervejas = sc.nextInt();
+		System.out.println("Digite a quantidade de refrigerantes: ");
+		int refrigerantes = sc.nextInt();
+		System.out.println("Digite a quantidade de espetinhos: ");
+		int espetinhos = sc.nextInt();
+		 
+
+		sc.close();
+
+
 
 	}
 
